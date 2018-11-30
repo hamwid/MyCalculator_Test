d1ae62ae26135229864fcc5c62f230342fb37a1c
hamwid <hampus.widell@alleman.nu>
2018-11-30 10:43:41 +0100

    Added tests for start-values of class variables

--- MyCalculator_Test.java
+++ MyCalculator_Test.java
@@ -12,12 +12,53 @@ public class MyCalculator_Test {
     }
     return false;
   }
+  private static boolean test_StartValueOfSecondNumber(){
+    double temp = calc.getSecondNumber();
+    if(0 == temp){
+      return true;
+    }
+    return false;
+  }
+  private static boolean test_StartValueOfAnswer(){
+    double temp = calc.getAnswer();
+    if(0 == temp){
+      return true;
+    }
+    return false;
+  }
+  private static boolean test_StartValueOfOperator(){
+    String temp = calc.getOperator();
+    if(temp == "-1"){
+      return true;
+    }
+    return false;
+  }
 
   public static void main(String[] args){
+    int totalAmountOfErros = 0;
     if(test_StartValueOfFirstNumber()){
-      System.out.println("test_StartValueOfFirstNumber: " + ANSI_GREEN + "PASSED"+ ANSI_RESET);
+      System.out.println("SV FirstNumber: " + ANSI_GREEN + "PASSED"+ ANSI_RESET);
     } else {
-      System.out.println("test_StartValueOfFirstNumber: " + ANSI_RED + "FAILED"+ ANSI_RESET);
+      System.out.println("SV FirstNumber: " + ANSI_RED + "FAILED"+ ANSI_RESET);
+      totalAmountOfErros++;
+    }
+    if(test_StartValueOfSecondNumber()){
+      System.out.println("SV SecondNumber: " + ANSI_GREEN + "PASSED"+ ANSI_RESET);
+    } else {
+      System.out.println("SV SecondNumber: " + ANSI_RED + "FAILED"+ ANSI_RESET);
+      totalAmountOfErros++;
+    }
+    if(test_StartValueOfAnswer()){
+      System.out.println("SV Answer: " + ANSI_GREEN + "PASSED"+ ANSI_RESET);
+    } else {
+      System.out.println("SV Answer: " + ANSI_RED + "FAILED"+ ANSI_RESET);
+      totalAmountOfErros++;
+    }
+    if(test_StartValueOfOperator()){
+      System.out.println("SV Operaotr: " + ANSI_GREEN + "PASSED"+ ANSI_RESET);
+    } else {
+      System.out.println("SV Operator: " + ANSI_RED + "FAILED"+ ANSI_RESET);
+      totalAmountOfErros++;
     }
   }
 }
